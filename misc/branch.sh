26c6e159f4ba3ae0a8c9e6afff38a3048aaab79b
Will Decker <107500716+w-decker@users.noreply.github.com>
2023-09-05 17:09:30 -0500

    Initial Overleaf Import

---
+++ misc/branch.sh
@@ -0,0 +1,1 @@
+$: git branch # tells you which branch you are on
---
+++ misc/switchbranch.sh
@@ -0,0 +1,4 @@
+$: git checkout -b new_branch # creates and switches to new branch
+$: git checkout main # reverts to main
+$: git checkout - # reverts to previous branch
+$: git checkout oldbranch # goes to another, already created branch
